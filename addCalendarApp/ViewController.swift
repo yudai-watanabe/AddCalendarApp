2a68c94653d201e4b21164b8ccefd7ac39e8e151
渡邊雄大＠SYS <watanabe-yudai@dmm.com>
2018-04-05 18:55:35 +0900

    update viewController

--- addCalendarApp/ViewController.swift
+++ addCalendarApp/ViewController.swift
@@ -39,16 +39,40 @@ class ViewController: UIViewController, MainViewDelegate {
 		}
 	}
 	
+	private func addEvent() {
+		let startDate = Date()
+		let cal = Calendar(identifier: Calendar.Identifier.gregorian)
+		let endDate = cal.date(byAdding: .hour, value: 2, to: startDate)
+		let title = "テストイベント"
+		let defaultCalendar = eventStore.defaultCalendarForNewEvents
+		
+		let event = EKEvent(eventStore: eventStore)
+		event.title = title
+		event.startDate = startDate
+		event.endDate = endDate
+		event.calendar = defaultCalendar
+		
+		do {
+			try eventStore.save(event, span: .thisEvent)
+		} catch let error {
+			print(error)
+		}
+	}
+	
 	// MARK: - MainViewDelegate
 	
 	func mainView(_ mainView: MainView, didTapped button: UIButton) {
 		guard self.isAuthed() else {
-			eventStore.requestAccess(to: .event, completion: { isCompleted, error in
-				print(isCompleted)
-				print(error as Any)
+			eventStore.requestAccess(to: .event, completion: {[weak self] granted, e in
+				if granted {
+					self?.addEvent()
+				} else {
+					print(e as Any)
+				}
 			})
 			return
 		}
+		self.addEvent()
 	}
 }
 
