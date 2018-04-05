4296037c686f365da6e71329c23027ccf2fe5b44
渡邊雄大＠SYS <watanabe-yudai@dmm.com>
2018-04-05 18:28:40 +0900

    update viewController

--- addCalendarApp/MainView.swift
+++ addCalendarApp/MainView.swift
@@ -7,20 +7,18 @@
 //
 
 import UIKit
-import EventKit
+
+protocol MainViewDelegate: NSObjectProtocol {
+	func mainView(_ mainView: MainView, didTapped button: UIButton)
+}
 
 class MainView: UIView {
 	
-	private let mainLabel: UILabel
-	
-	private let button: UIButton
-	
-	private let eventStore = EKEventStore()
+	private let mainLabel = UILabel()
+	private let button = UIButton()
+	weak var delegate: MainViewDelegate?
 	
 	override init(frame: CGRect) {
-		self.mainLabel = UILabel()
-		self.button = UIButton()
-		
 		super.init(frame: frame)
 		self.backgroundColor = .white
 		self.button.addTarget(self, action: #selector(self.tapped(_:)), for: .touchUpInside)
@@ -56,13 +54,14 @@ class MainView: UIView {
 		self.button.setTitleColor(.white, for: .normal)
 		self.button.layer.masksToBounds = true
 		self.button.backgroundColor = .blue
+		self.button.setTitleShadowColor(nil, for: .highlighted)
 		
 		let buttonSize = self.button.sizeThatFits(self.bounds.size)
 		let buttonX = (self.bounds.width - buttonSize.width) / 2
 		let buttonY = (self.bounds.height - buttonSize.height) / 2
 		let buttonOrigin = CGPoint(x: buttonX, y: buttonY + 100)
-		
 		self.button.layer.cornerRadius = buttonSize.height / 2
+		
 		self.button.frame = CGRect(origin: buttonOrigin, size: buttonSize)
 	}
 	
@@ -70,14 +69,7 @@ class MainView: UIView {
 		mainLabel.text = title
 	}
 	
-	@objc internal func tapped(_ sender: AnyObject) {
-		guard EKEventStore.authorizationStatus(for: .event) == .authorized else {
-			eventStore.requestAccess(to: .event, completion: { isCompleted, error in
-				print(isCompleted)
-				print(error as Any)
-			})
-			return
-		}
-		
+	@objc internal func tapped(_ button: UIButton) {
+		delegate?.mainView(self, didTapped: button)
 	}
 }
--- addCalendarApp/ViewController.swift
+++ addCalendarApp/ViewController.swift
@@ -9,27 +9,46 @@
 import UIKit
 import EventKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MainViewDelegate {
+	
+	private let eventStore = EKEventStore()
 	
 	override func viewDidLoad() {
-		
 		super.viewDidLoad()
-		
 		let mainView = MainView(frame: self.view.bounds)
-		
+		mainView.delegate = self
 		mainView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-		
 		mainView.setTitle(title: "Add Calendar App")
 		
 		self.view.addSubview(mainView)
-		
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	// MARK: - Private
+	
+	private func isAuthed() -> Bool {
+		switch EKEventStore.authorizationStatus(for: .event) {
+		case .authorized: return false
+		case .denied: return false
+		case .notDetermined: return false
+		case .restricted: return true
+		}
+	}
+	
+	// MARK: - MainViewDelegate
+	
+	func mainView(_ mainView: MainView, didTapped button: UIButton) {
+		guard self.isAuthed() else {
+			eventStore.requestAccess(to: .event, completion: { isCompleted, error in
+				print(isCompleted)
+				print(error as Any)
+			})
+			return
+		}
+	}
 }
 
